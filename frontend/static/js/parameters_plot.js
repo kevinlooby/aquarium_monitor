e08d60abd3cbd0eec37a00776f692c8e5f574d22
Kevin Looby <kevinlooby@gmail.com>
2023-02-01 17:57:37 -0800

    Added placeholder Highcharts call

---
+++ frontend/static/js/parameters_plot.js
@@ -0,0 +1,33 @@
+
+$(document).ready(function () {
+
+    $.ajax({
+        dataType: "json",
+        url: "http://localhost:8090/NOTIMPLEMENTED"
+        success: function (data) {
+
+            $("#plot_spinner").remove()
+
+            // Highcharts
+            $("#parameters").append('<div id="parameters_plot" style="height: 400px; min-width: 310px"></div>')
+
+            Highcharts.stockChart('parameters_plot', {
+                rangeSelector: {selected: 2},
+                title: {text: 'Water Parameters'},
+                chart: {displayErrors: true},
+                xAxis: {
+                    tickInterval: 24 * 3600 * 1000,
+                    type: 'datetime',
+                }
+                series: [
+                    {name: 'pH'},
+                    data: data['pH'],
+                    lineWidth: 1,
+                    tooltip: {valueDecimals: 1},
+                ]
+            })
+
+        }
+    })
+
+})
