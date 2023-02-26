939fc65f525d51e6270c8ceac47872690a1396ae
Kevin Looby <kevinlooby@gmail.com>
2023-02-25 18:19:42 -0800

    Fixed ph probe voltage value

--- sensing/ph/PHSensorI2C.py
+++ sensing/ph/PHSensorI2C.py
@@ -14,7 +14,7 @@ class PHSensorI2C:
         self.ads1115.set_gain(ADS1115_REG_CONFIG_PGA_6_144V)
         adc0 = self.ads1115.read_voltage(0)
         ph = self.ph_sensor.read_PH(adc0['r'], temperature)
-        return ph, adc0
+        return ph, adc0['r']
 
 
 class TempSensorI2C:
